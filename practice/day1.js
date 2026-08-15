289030e9ae345c519184266e6b6437dc89eba9e0
Shaun Ng <shaunng_ys@proton.me>
2026-08-15 15:55:59 +0800

    Added 4 more completed exercises to day1.js file

--- practice/day1.js
+++ practice/day1.js
@@ -52,3 +52,64 @@ function largest(array) {
 }
 
 //console.log(largest([3, 9, 2, 7]));
+
+//5. Count the vowels in a string
+function countVowels(str) {
+  let counter = 0;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (
+      str[i] == "a" ||
+      str[i] == "A" ||
+      str[i] == "e" ||
+      str[i] == "E" ||
+      str[i] == "i" ||
+      str[i] == "I" ||
+      str[i] == "o" ||
+      str[i] == "O" ||
+      str[i] == "u" ||
+      str[i] == "U"
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+//console.log(countVowels("eEAauJEsTeraaAuu"));
+
+//6. Check whether a string is a palindrome
+function isPalindrome(str) {
+  if (reverseString(str) == str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//console.log(isPalindrome("!racecaR!"));
+//console.log(isPalindrome("hello"));
+
+//7. Convert Celsius to Fahrenheit
+function toFahrenheit(tempC) {
+  let tempF;
+
+  tempF = tempC * 1.8 + 32;
+  return tempF;
+}
+
+//console.log(toFahrenheit(0));
+//console.log(toFahrenheit(100));
+
+//8. Count how many times a character appears in a string
+function countChar(str, char) {
+  let counter = 0;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] == char) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(countChar("banana", "a"));
+console.log(countChar("banana", "z"));
