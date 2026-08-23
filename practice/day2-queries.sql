929651e1fe1fa355eccc26b0c47727dab748a3e1
Shaun Ng <shaunng_ys@proton.me>
2026-08-23 23:52:14 +0800

    Adds 5 more query lines

--- practice/day2-queries.sql
+++ practice/day2-queries.sql
@@ -1,1 +1,6 @@
 SELECT * FROM applications ORDER BY applied_date DESC;
+SELECT * FROM applications WHERE status = 'interview' ORDER BY applied_date DESC;
+SELECT * FROM applications WHERE id = 3;
+SELECT status, COUNT(*) FROM applications GROUP BY status;
+SELECT COUNT(*) FROM applications WHERE status != 'wishlist';
+SELECT COUNT(*) FROM applications WHERE status IN ('screening', 'interview', 'offer');
