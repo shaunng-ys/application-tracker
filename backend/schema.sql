ccdbb2a3c294cff52f79ee1d4e9ebd310d120644
Shaun Ng <shaunng_ys@proton.me>
2026-08-29 19:52:35 +0800

    Fixes schema so it runs, corrects data types, adds constraints to it and adds test queries to practice file

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE applications (
-	id TEXT,
-	company TEXT,
-	role TEXT,
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	company TEXT NOT NULL,
+	role TEXT NOT NULL,
 	source TEXT,
 	url TEXT,
 	salary_min INT,
 	salary_max INT,
-	status TEXT,
+	status TEXT CHECK (status IN ('not applied', 'applied', 'screening', 'interview', 'offer', 'rejected', 'ghosted')) NOT NULL,
 	applied_date DATE,
 	notes TEXT,
-	created_at DATE,
+	created_at TIMESTAMPTZ DEFAULT now(),
 	cv_version TEXT,
 	referral_source TEXT,
 	follow_up_date DATE,
-	user_id TEXT,
+	user_id INT
 );
--- practice/day2-queries.sql
+++ practice/day2-queries.sql
@@ -8,3 +8,26 @@ SELECT company, role, follow_up_date FROM applications WHERE follow_up_date <= C
 SELECT * FROM applications WHERE company ILIKE '%bank%';\
 SELECT company, role, salary_max FROM applications ORDER BY salary_max DESC LIMIT 5;
 SELECT referral_source, COUNT(*) FROM applications WHERE referral_source IS NOT NULL GROUP BY referral_source;
+
+INSERT INTO applications (company, role, status)
+VALUES
+	('shopee', 'software dev', 'not applied'),
+    ('grab', 'junior dev', 'applied'),
+    ('gx bank', 'web developer', 'not applied'),
+    ('maybank', 'software engineer', 'applied'),
+    ('uob', 'junior software engineer', 'not applied'),
+    ('moneylion', 'Software Engineer I', 'applied'),
+    ('ryt bank', 'junior web developer', 'not applied'),
+    ('lazada', 'software engineer', 'applied'),
+    ('maxis', 'junior developer', 'not applied'),
+    ('BASF', 'graduate trainee', 'applied');
+
+SELECT * FROM applications;
+
+UPDATE applications SET applied_date = CURRENT_DATE WHERE status = 'applied';
+
+SELECT * FROM applications;
+
+DELETE FROM applications WHERE company = 'BASF';
+
+SELECT * FROM applications;
