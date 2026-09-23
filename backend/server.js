0adbff6c91e74dfa412c52868d13a0dde8472e45
Shaun Ng <shaunng_ys@proton.me>
2026-09-24 04:17:45 +0800

    Adds GET request handler where the server talks to database

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,37 @@
+//server.js
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import pool from "./db.js";
 
 const app = express();
 const port = 3000;
 
+/*
 const applications = [
   { id: 1, company: `TnG`, role: `Software Dev`, status: `applied` },
   { id: 2, company: `Shopee`, role: `Software Engineer`, status: `applied` },
   { id: 3, company: `Grab`, role: `Web Dev`, status: `applied` },
 ];
+*/
 
 app.use(express.json());
 app.use(cors());
 
+/*
 app.get("/api/applications", (req, res) => {
   res.json(applications);
+});
+*/
+
+app.get("/api/applications", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM applications");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("FULL DATABASE ERROR:", err);
+    res.status(500).send("Database error");
+  }
 });
 
 app.listen(process.env.PORT || port, () => {
